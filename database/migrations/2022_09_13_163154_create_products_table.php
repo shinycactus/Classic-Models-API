20ed460b28b78664f35e3ad3fee6b762ffccb3d4
Joe <joe.clarke@swiftqueue.com>
2022-09-15 17:37:43 +0100

    Product line seeder

---
+++ database/migrations/2022_09_13_163154_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_code', 15);
+            $table->string('product_name', 70);
+            $table->unsignedBigInteger('product_line_id');
+            $table->string('product_scale', 10);
+            $table->string('product_vendor', 50);
+            $table->text('product_description');
+            $table->smallInteger('quantity_in_stock');
+            $table->decimal('buy_price', 10, 2);
+            $table->decimal('msrp', 10, 2);
+            $table->timestamps();
+
+            $table->foreign('product_line_id')->references('id')->on('product_lines');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
